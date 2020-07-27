dfbc19f87b653b29e285d645450d0767d117679e
Leandro Scardua <leandroscar@gmail.com>
2020-07-26 17:50:44 -0700

    Create install-docker-compose_pt_br.ps1

---
+++ install-docker-compose_pt_br.ps1
@@ -0,0 +1,5 @@
+# Verificar a ultima versao disponivel do docker-compose
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$tag = (Invoke-WebRequest "https://api.github.com/repos/docker/compose/releases/latest" | ConvertFrom-Json)[0].tag_name
+# Baixar e instalar a ultima versao disponvel fo docker-compose.
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/$tag/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\Docker\docker-compose.exe
